8c37d2185cebb3ab88826a06bb6485054c3ec5a9
moongoblin <goblinfromdahmoon@gmail.com>
2025-04-07 20:37:17 +0300

    run backend and frontend in docker

--- Backend/main.py
+++ Backend/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 import uvicorn
 from routes.profile import profile
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="Приложение для знакомств", description="Познакомьтесь с интересными людьми")
 
@@ -9,7 +10,6 @@ app.include_router(profile)
 # --------------------------
 # enable cors, or cross origin request something
 # allows frontend to talk to backend
-from fastapi.middleware.cors import CORSMiddleware
 #
 # app = FastAPI()
 
@@ -27,6 +27,6 @@ app.add_middleware(
 )
 # --------------------------
 
-# i specified 0.0.0.0 because 127.0.0.1 cannot be reached outside of the docker container
+# i specified 0.0.0.0 because apparently 127.0.0.1 cannot be reached outside of the docker container
 if __name__ == "__main__":
     uvicorn.run(app, port=8000, host='0.0.0.0')
--- Backend/settings.py
+++
@@ -1,7 +0,0 @@
-from pydantic_settings import BaseSettings
-
-class Settings(BaseSettings):
-    DB_DSN: str = ""
-
-def get_settings():
-    return Settings()
